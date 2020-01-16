2599c477ac78a2982664c5477c9b812a1426fbba
brmiranda <brmiranda@fs.fed.us>
2020-01-16 15:28:56 -0600

    Reorganized file locations. Updated for compatibility with v7. Updated example files. Compiled v1.0.

---
+++ src/ClimateMapFileNames.cs
@@ -0,0 +1,52 @@
+//  Copyright 2005-2010 Portland State University, University of Wisconsin-Madison
+//  Authors:  Robert M. Scheller, Jimm Domingo
+
+
+using Landis.Utilities;
+using Landis.Core;
+using System.Collections.Generic;
+
+namespace Landis.Extension.Output.LandscapeHabitat
+{
+    /// <summary>
+    /// Methods for working with the template for filenames of reclass maps.
+    /// </summary>
+    public static class ClimateMapFileNames
+    {
+        public const string MapNameVar = "climate-var-name";
+        public const string TimestepVar = "timestep";
+
+
+        private static IDictionary<string, bool> knownVars;
+        private static IDictionary<string, string> varValues;
+
+        //---------------------------------------------------------------------
+
+        static ClimateMapFileNames()
+        {
+            knownVars = new Dictionary<string, bool>();
+            knownVars[MapNameVar] = true;
+            knownVars[TimestepVar] = true;
+
+            varValues = new Dictionary<string, string>();
+        }
+
+        //---------------------------------------------------------------------
+
+        public static void CheckTemplateVars(string template)
+        {
+            OutputPath.CheckTemplateVars(template, knownVars);
+        }
+
+        //---------------------------------------------------------------------
+
+        public static string ReplaceTemplateVars(string template,
+                                                 string reclassMapName,
+                                                 int    timestep)
+        {
+            varValues[MapNameVar] = reclassMapName;
+            varValues[TimestepVar] = timestep.ToString();
+            return OutputPath.ReplaceTemplateVars(template, varValues);
+        }
+    }
+}
---
+++ src/SpeciesHabitatLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Landis.Library.Metadata;
+
+namespace Landis.Extension.Output.LandscapeHabitat
+{
+    
+    public class SpeciesHabitatLog
+    {
+        [DataFieldAttribute(Unit = FieldUnits.Year, Desc = "Simulation Year")]
+        public int Time { set; get; }
+
+        [DataFieldAttribute(Desc = "Ecoregion Name")]
+        public string Ecoregion { set; get; }
+
+        [DataFieldAttribute(Desc = "Species Model Name")]
+        public string SpeciesModelName { set; get; }
+
+        [DataFieldAttribute(Unit = "Index", Desc = "Index", Format = "0.00")] 
+        public double Index { set; get; }
+        
+    }
+}
